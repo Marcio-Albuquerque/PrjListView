dd1d04b20ce635bdcb7757fd886d193815b46d5c
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2016-04-27 17:10:40 -0300

    Completado a activity -> single_list_item_view.xml e SingleListItem.Java

--- app/src/main/java/com/example/marcioalbuquerque/prjlistview/SingleListItem.java
+++ app/src/main/java/com/example/marcioalbuquerque/prjlistview/SingleListItem.java
@@ -1,13 +1,22 @@
 package com.example.marcioalbuquerque.prjlistview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SingleListItem extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.single_list_item_view);
+        this.setContentView(R.layout.single_list_item_view);
+        TextView txtPais = (TextView) findViewById(R.id.pais_label);
+        Intent i = getIntent();
+        String pais = i.getStringExtra("pais");
+        // Mostrar o nome do país selecionado
+        txtPais.setText(pais);
+
     }
 }
+
