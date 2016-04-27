625a2d19f653d142659d2b1c9f2ca3afec45d19f
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2016-04-27 17:07:12 -0300

    Finalizando do  lis view java
    Criando a activity -> single_list_item_view.xml e SingleListItem.Java

--- app/src/main/java/com/example/marcioalbuquerque/prjlistview/ListViewActivity.java
+++ app/src/main/java/com/example/marcioalbuquerque/prjlistview/ListViewActivity.java
@@ -1,13 +1,47 @@
 package com.example.marcioalbuquerque.prjlistview;
 
+import android.annotation.SuppressLint;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.suitebuilder.annotation.Suppress;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class ListViewActivity extends AppCompatActivity {
+@SuppressLint("ParserError")
+public class ListViewActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        //Copiar strings para Array
+        String[] listaPaises = getResources().getStringArray(R.array.paises);
+
+        //Fazer uma ligação do Array(listaPaises) para um ListAdapter
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, listaPaises));
+
+        ListView lv = getListView();
+
+        //Lidar com o clique de um item da lista
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //Item selecionado
+            String pais = ((TextView)view).getText().toString();
+            Long.d("DEBUG", pais);
+            //Invocar nova atividade após seleção de item
+            Intent intent = new Intent(getApplicationContext(), SingleListItem.class);
+            //Enviar dados para a nova atividade
+            intent.putExtra("pais", pais);
+            startActivity(intent);
+            }
+        });
+        }
     }
-}
+
